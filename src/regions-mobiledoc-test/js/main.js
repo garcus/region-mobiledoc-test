b356ee846c8c6eaf2eec46374914837cc7554930
Marcus Kovács <vimks@vitec.se>
2015-12-08 09:25:42 +0100

    Fix side by side editing with live update of text flow.

--- src/regions-mobiledoc-test/js/main.js
+++ src/regions-mobiledoc-test/js/main.js
@@ -1,15 +1,30 @@
 $(document).ready(function () {
     $( "#editStory" ).click(editStory);
-    $( "#editor-save" ).click(saveStory);
+    $( "#editor-save" ).click(saveStoryAndClose);
     $("#story").children("*").clone().appendTo("#content");
     $("#story").children("*").clone().appendTo("#editor-content");
 
+    var timer;
+    $('#editor-content').keyup(function () {
+        clearTimeout(timer);
+        timer = setTimeout(function (event) {
+            saveStory();
+        }, 500);
+    });
+
+
     function editStory() {
         $("#editor").show();
+        $("#editStory").attr('disabled','disabled');
+    }
+
+    function saveStoryAndClose() {
+        saveStory();
+        $("#editor").hide();
+        $("#editStory").removeAttr('disabled');
     }
 
     function saveStory() {
-        $("#editor").hide();
         var content = $("#editor-content").html();
         $("#content").html(content);
     }
